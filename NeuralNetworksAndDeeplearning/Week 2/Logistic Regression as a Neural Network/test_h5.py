7a467d345852376687c35bb124c1cb0f5909c70f
fan chuankang <flavorfan@tencent.com>
2018-01-13 21:24:23 +0800

    week 6

--- NeuralNetworksAndDeeplearning/Week 2/Logistic Regression as a Neural Network/test_h5.py
+++ NeuralNetworksAndDeeplearning/Week 2/Logistic Regression as a Neural Network/test_h5.py
@@ -1,3 +1,8 @@
-from lr_utils import load_dataset
+# from lr_utils import load_dataset
+import numpy as np
+import h5py
 
-train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
+f = h5py.File('datasets/train_catvnoncat.h5', "r")
+f.keys()
+
+# train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
